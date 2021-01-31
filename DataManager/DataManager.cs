06f8ef01c2878412ba1ecbde7d71e0770d49eb3b
aliardan <33637478+aliardan@users.noreply.github.com>
2021-01-31 18:42:37 +0300

    Basic implementation

---
+++ DataManager/DataManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YandexDisk.Client;
+using YandexDisk.Client.Clients;
+using YandexDisk.Client.Http;
+
+namespace DataManager
+{
+    class DataManager
+    {
+        public async Task UploadAsync(string fileName, string yandexDiskTarget)
+        {
+            IDiskApi diskApi = new DiskHttpApi("AgAAAABQNcSVAADLW8Rn_gQcNkrhlIhLGyzFvWM");
+            var targtPath = Path.TrimEndingDirectorySeparator(yandexDiskTarget) + "/"+ Path.GetFileName(fileName);
+
+            await diskApi.Files.UploadFileAsync(targtPath, false, fileName, CancellationToken.None);
+        }
+    }
+}
---
+++ DataManager/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DataManager
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                throw new ArgumentException("Wrong arguments count");
+            }
+
+            DataManager dataManager = new DataManager();
+            var files = Directory.GetFiles(args[0]);
+            Console.WriteLine("Files are being uploaded");
+            int counter = 1;
+            foreach (var file in files)
+            {
+                Console.WriteLine($"File {file} is being uploaded ({counter}/{files.Length})");
+                await dataManager.UploadAsync(file, args[1]);
+                counter++;
+            }
+
+            Console.WriteLine("All files have been uploaded");
+        }
+    }
+}
